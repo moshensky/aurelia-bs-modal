b6d485fd58832e1defb0120b1bed9ea0c318bda4
Nikita Moshensky <nikita@moshensky.com>
2015-09-30 12:37:30 +0300

    Update modal-header.js

--- src/modal-header.js
+++ src/modal-header.js
@@ -3,6 +3,7 @@ import {bindable} from 'aurelia-framework';
 export class ModalHeader {
   @bindable title = '';
   @bindable close = closeModal;
+  @bindable icon = '';
 }
 function closeModal(){
   console.log('Please bind a function to close modal - "<modal-header close.call="myFunc()"></modal-header>');
